2ce18994ba136a2ec6e6cc6956ab3b6bf1f8eca9
ddiyoo <ddiyoo@gmail.com>
2020-11-16 20:24:26 +0900

    Add level_1 folder
    level_1 folder : programmers level 1 exam

---
+++ level_1/combination.py
@@ -0,0 +1,15 @@
+from itertools import combinations
+numbers = [2,1,3,4,1]
+
+def solution(numbers):
+    a = list(combinations(numbers, 2))
+    answer = []
+    for i in range(len(a)):    ### i in a 로 했으면 더 간단
+        answer.append(a[i][0] + a[i][1])  ### i[0]+i[1]로 할 수 있음
+    answer = list(set(answer))
+    answer = sorted(answer)   ### answer.sort()
+
+    return answer
+
+print(solution(numbers))
+
