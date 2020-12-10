90e2d6c402016e8edbc287bf7625256a9a30aa7e
ddiyoo <ddiyoo@gmail.com>
2020-12-10 16:17:52 +0900

    Modified algorithm/recursive_function.py
    solve binary_search_applied_recursive_function

--- algorithm/recursive_function.py
+++ algorithm/recursive_function.py
@@ -74,6 +74,24 @@ def binary_search(element, some_list, start_index=0, end_index=None):
     if end_index == None:
         end_index = len(some_list) - 1
 
+    if start_index == end_index:
+        return None
+
+    mid_index = (start_index + end_index) // 2
+
+    if some_list[mid_index] == element:
+        return mid_index
+    elif some_list[mid_index] > element:
+        start_index = 0
+        end_index = mid_index
+        return binary_search(element, some_list, start_index=0, end_index=None)
+    elif some_list[mid_index] < element:
+        start_index = mid_index
+        end_index = len(some_list)
+        return binary_search(element, some_list, start_index=0, end_index=None)
+
+
+
     # 코드를 작성하세요.
 
 print(binary_search(2, [2, 3, 5, 7, 11]))
@@ -81,3 +99,33 @@ print(binary_search(0, [2, 3, 5, 7, 11]))
 print(binary_search(5, [2, 3, 5, 7, 11]))
 print(binary_search(3, [2, 3, 5, 7, 11]))
 print(binary_search(11, [2, 3, 5, 7, 11]))
+
+#
+#
+# def binary_search(element, some_list, start_index=0, end_index=None):
+#     # end_index가 따로 주어지지 않은 경우에는 리스트의 마지막 인덱스
+#     if end_index == None:
+#         end_index = len(some_list) - 1
+#
+#     # 코드를 작성하세요.
+#     if start_index == end_index:
+#         return None
+#
+#     mid_index = (start_index + end_index) // 2
+#
+#     if some_list[mid_index] == element:
+#         return mid_index
+#     elif some_list[mid_index] > element:
+#         start_index = 0
+#         end_index = mid_index
+#         return binary_search(element, some_list, start_index, end_index)
+#     elif some_list[mid_index] < element:
+#         start_index = mid_index
+#         end_index = len(some_list)
+#         return binary_search(element, some_list, start_index, end_index)
+#
+# print(binary_search(2, [2, 3, 5, 7, 11]))
+# print(binary_search(0, [2, 3, 5, 7, 11]))
+# print(binary_search(5, [2, 3, 5, 7, 11]))
+# print(binary_search(3, [2, 3, 5, 7, 11]))
+# print(binary_search(11, [2, 3, 5, 7, 11]))
